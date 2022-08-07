16f205a0c37e9926f4a271e11bfda29caa5d61b8
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-08-07 11:48:24 +0100

    complete evenorodd

---
+++ 8kyu/evenOrOdd.js
@@ -0,0 +1,13 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+    return number % 2 == 0 ? "Even" : "Odd"
+}
+
+// REFACTOR
+
+const even_or_odd = number => number % 2 == 0 ? "Even" : "Odd";
+
+
+
+console.log(even_or_odd(5))
