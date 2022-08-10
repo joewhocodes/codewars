8f5b58e7b0e853bfaafdd46821fa72098193bc58
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-08-10 17:00:07 +0100

    complete remove-the-minimum

--- 8kyu/how-good-are-you.js
+++ 8kyu/how-good-are-you.js
@@ -1,5 +1,6 @@
-const betterThanAverage = (classPoints, yourPoints) => classPoints.reduce((a, b) => a + b) / classPoints.length > yourPoints ? false : true;
+const betterThanAverage = (classPoints, yourPoints) => classPoints.reduce((a, b) => a + b) / classPoints.length < yourPoints;
 
+console.log(10 > 4)
 
 // console.log(betterThanAverage([2, 3], 5), true)
 console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75), true);
