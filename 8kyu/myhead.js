ea01a47c04ae31bd8ac00a58f7a610e5bd7be3d2
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-08-20 11:17:30 +0100

    complete myhead

---
+++ 8kyu/myhead.js
@@ -0,0 +1,5 @@
+// You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+const fixTheMeerkat = arr => arr.reverse();
+
+console.log(fixTheMeerkat(["tail", "body", "head"]))
