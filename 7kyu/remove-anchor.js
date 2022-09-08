bdd7ba876cee96b9d14298f7ade336e1b0df1733
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-09-08 09:29:11 +0100

    complete remove-anchor

---
+++ 7kyu/remove-anchor.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+
+const removeUrlAnchor = url => url.split("#"[0])[0];
+
+
+console.log(removeUrlAnchor('www.codewars.com#about'))
