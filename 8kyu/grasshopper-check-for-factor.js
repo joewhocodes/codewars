309687afb24cb268df94ab9671439d6fdc75db41
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-09-19 22:38:12 +0100

    complete grasshopper-check-for-factor

---
+++ 8kyu/grasshopper-check-for-factor.js
@@ -0,0 +1,6 @@
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+
+const checkForFactor = (base, factor) => base % factor === 0 ? true : false;
