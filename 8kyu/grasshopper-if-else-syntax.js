15849a9fd50814283db328ccd0b57cde2cc4b7db
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-09-20 09:59:30 +0100

    complete grasshopper-if-else-syntax

---
+++ 8kyu/grasshopper-if-else-syntax.js
@@ -0,0 +1,5 @@
+// While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
+
+// checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
+
+const checkAlive = health => health > 0;
