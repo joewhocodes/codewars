ee3914aac85c15c8db392329c33fb02412e32e3a
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2022-12-02 07:22:01 -0600

    complete shorter-concat

---
+++ 7kyu/shorter-concat.js
@@ -0,0 +1,16 @@
+// Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+
+// In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+
+// Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+// If a and b have the same length treat a as the longer producing b+reverse(a)+b
+
+// always 2 strings, can be empty, 
+// return str
+
+const shorter_reverse_longer = (a, b) => {
+    const sorted = (a.length == b.length) ? [b, a] : [...arguments].sort((a, b) => a.length - b.length);
+    return sorted[0] + sorted[1].split('').reverse().join('') + sorted[0];
+} 
+
+console.log(shorter_reverse_longer("first", "abcde"))
