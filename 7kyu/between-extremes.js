8e6bd775f24ee2fc842c3c5d4970b8aeb79a06cf
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2023-04-08 09:06:19 -0300

    complete between-extremes

---
+++ 7kyu/between-extremes.js
@@ -0,0 +1,12 @@
+// write a function that caculates the difference between the biggest and smallest element of array
+// array of nums, always valid, always positive
+// return num
+// 
+
+const betweenExtremes = n => {
+    const sorted = n.sort((a, b) => a - b);
+    return sorted[n.length - 1] - sorted[0];
+};
+
+console.log(betweenExtremes([21, 34, 54, 43, 26, 12]), 42);
+console.log(betweenExtremes([-1, -41, -77, -100]), 99);
