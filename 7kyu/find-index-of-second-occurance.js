b6e7c804ae2fa3eece1d5264be81991bdb8cf358
Joe Ulyatt <joeulyatt1@hotmail.co.uk>
2023-04-30 08:15:06 +0100

    complete find-index-of-second-occurance

---
+++ 7kyu/find-index-of-second-occurance.js
@@ -0,0 +1,8 @@
+// function, finds the index of 2nd occurance of string, if no match return -1
+// String, inc special chars
+// return num
+
+const secondSymbol = (str, sym) => str.indexOf(sym, str.indexOf(sym) + 1);
+
+console.log(secondSymbol('Hello world!!!','l'), 3)
+console.log(secondSymbol('Hello world!!!','A'), -1)
